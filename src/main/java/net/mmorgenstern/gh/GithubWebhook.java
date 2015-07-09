76247c7d100b335ed8e063d85879b79e2a59b13c
Martin Morgenstern <mail@mmorgenstern.net>
2015-07-09 23:20:27 +0200

    Refactor the code and cleanup gradle file
    * get rid of Logger, Mockito and HttpServletRequest
    * remove commons-io and mockito deps

--- src/main/java/net/mmorgenstern/gh/GithubWebhook.java
+++ src/main/java/net/mmorgenstern/gh/GithubWebhook.java
@@ -1,20 +1,15 @@
 package net.mmorgenstern.gh;
 
-import java.io.IOException;
 import java.util.Objects;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.commons.codec.digest.HmacUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,8 +17,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class GithubWebhook {
+    private static final String EOL = "\n";
     private final String secret;
-    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public GithubWebhook() {
         this(System.getenv("SECRET_KEY"));
@@ -41,24 +36,23 @@ public class GithubWebhook {
     }
 
     @RequestMapping(value = "/github-webhook", method = RequestMethod.POST)
-    public ResponseEntity<String> receiveHook(HttpServletRequest request,
-            @RequestBody String payload) throws IOException {
-        String signature = request.getHeader("X-Hub-Signature");
-
+    public ResponseEntity<String> handle(@RequestHeader("X-Hub-Signature") String signature,
+            @RequestBody String payload) {
         if (signature == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No signature given.\n");
+            return ResponseEntity.badRequest().body("No signature given." + EOL);
         }
 
         String computed = String.format("sha1=%s", HmacUtils.hmacSha1Hex(secret, payload));
 
-        logger.info("signature: {}", signature);
-        logger.info("computed: {})", computed);
-
         if (!StringUtils.constantTimeCompare(signature, computed)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid signature.\n");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid signature." + EOL);
         }
 
-        return ResponseEntity.ok().contentType(MediaType.TEXT_PLAIN).body(payload);
+        int bytes = payload.getBytes().length;
+        StringBuilder message = new StringBuilder();
+        message.append("Signature ok.").append(EOL);
+        message.append(String.format("Received %d bytes.", bytes)).append(EOL);
+        return ResponseEntity.ok().body(message.toString());
     }
 
     public static void main(String[] args) {
--- src/test/java/net/mmorgenstern/gh/GithubWebhookTest.java
+++ src/test/java/net/mmorgenstern/gh/GithubWebhookTest.java
@@ -3,54 +3,42 @@ package net.mmorgenstern.gh;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class GithubWebhookTest {
-    @Mock
-    private HttpServletRequest request;
     private GithubWebhook hook;
 
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
         hook = new GithubWebhook("secret");
     }
 
     @Test
     public void testNoSignature() throws IOException {
-        when(request.getHeader(anyString())).thenReturn(null);
-        ResponseEntity<String> entity = hook.receiveHook(request, "hello");
+        ResponseEntity<String> entity = hook.handle(null, "hello");
         assertThat(entity.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
         assertThat(entity.getBody(), containsString("No signature"));
     }
 
     @Test
     public void testPayloadModified() throws IOException {
-        when(request.getHeader("X-Hub-Signature"))
-                .thenReturn("sha1=5112055c05f944f85755efc5cd8970e194e9f45b");
-        ResponseEntity<String> entity = hook.receiveHook(request, "evil hello");
+        ResponseEntity<String> entity = hook
+                .handle("sha1=5112055c05f944f85755efc5cd8970e194e9f45b", "evil hello");
         assertThat(entity.getStatusCode(), equalTo(HttpStatus.UNAUTHORIZED));
         assertThat(entity.getBody(), containsString("Invalid"));
     }
 
     @Test
     public void testValidSignature() throws IOException {
-        when(request.getHeader("X-Hub-Signature"))
-                .thenReturn("sha1=5112055c05f944f85755efc5cd8970e194e9f45b");
-        ResponseEntity<String> entity = hook.receiveHook(request, "hello");
+        ResponseEntity<String> entity = hook
+                .handle("sha1=5112055c05f944f85755efc5cd8970e194e9f45b", "hello");
         assertThat(entity.getStatusCode(), equalTo(HttpStatus.OK));
-        assertThat(entity.getBody(), equalTo("hello"));
+        assertThat(entity.getBody(), containsString("Signature ok"));
     }
 }
